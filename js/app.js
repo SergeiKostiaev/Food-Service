ba4b38210b112e5c609227fa86dd06184a4d360b
KostiaevSV <skprod.info@gmail.com>
2023-04-29 23:45:26 +0400

    add any pages & created js rout

--- js/app.js
+++ js/app.js
@@ -1,10 +1,34 @@
 /****************/
 /*Все переменные*/
 /************** */
-
-
-
+const tabs = document.querySelectorAll(".first_nav");
+const contents = document.querySelectorAll(".info");
+const btnActive = document.getElementsByClassName("nav_link")
+ 
 /*Все функции*/
+
+for (let i = 0; i < tabs.length; i++) {
+	tabs[i].addEventListener("click", ( event ) => {
+ 
+		//удаляем активный класс с вкладок
+		let tabsChildren = event.target.parentElement.children;
+		for (let t = 0; t < tabsChildren.length; t++) {
+			tabsChildren[t].classList.remove("tab_active");
+		}
+		// добавляем активный класс
+		tabs[i].classList.add("tab_active");
+		//удаляем активный класс с блоков содержимого
+		let tabContentChildren = event.target.parentElement.nextElementSibling.children;
+		for (let c = 0; c < tabContentChildren.length; c++) {
+			tabContentChildren[c].classList.remove("content_active");
+		}
+		// добавляем активный класс
+		contents[i].classList.add("content_active");
+ 
+	});
+}
+
+
 function myFunction() {
     let x = document.getElementById("nav");
     if (x.className === "nav") {
